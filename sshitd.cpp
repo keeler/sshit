d28b23c10af3132d859d5688a67b5c877aa7deda
Keeler Russell <keeler-russell@ku.edu>
2013-02-08 17:15:24 -0600

    Fixed issues with sshitd daemon file writing. Added syslog().

--- sshitd.cpp
+++ sshitd.cpp
@@ -6,6 +6,7 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <syslog.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -18,7 +19,7 @@
 #include <fstream>
 using namespace std;
 
-#define PORT_NUMBER "9876"
+#define PORT_NUMBER 12346
 #define MAX_BUFFER_SIZE 1024
 
 void sigchld_handler( int s )
@@ -43,91 +44,83 @@ int main()
 		exit( EXIT_SUCCESS );
 	}
 
+	openlog( "sshitd", 0, LOG_USER );
+	
 	// Child process becomes the daemon
 	umask( 0 );
-
-	ofstream logfile( "sshitd.log", ios::out );
-	if( logfile.fail() )
-	{
-		cerr << "Couldn't set up log file" << endl;
-		exit( EXIT_FAILURE );
-	}
-
+	
 	pid_t sid = setsid();
 	if( sid < 0 )
 	{
-		logfile << "setsid() failure." << endl;
-		logfile.close();
+		syslog( LOG_INFO, "%s", "setsid() failure" );
 		exit( EXIT_FAILURE );
 	}
 
-	if( chdir( "/" ) < 0 )
+	if( chdir( getenv( "HOME" ) ) < 0 )
 	{
-		logfile << "chdir() failure." << endl;
-		logfile.close();
+		syslog( LOG_INFO, "%s", "chdir() failure" );
 		exit( EXIT_FAILURE );
 	}
-
+	
 	close( STDIN_FILENO );
 	close( STDOUT_FILENO );
 	close( STDERR_FILENO );
 
+	syslog( LOG_INFO, "%s", "daemon setup complete" );
+
 	int handshake_socket;
-	struct addrinfo hints;
-	struct addrinfo *servinfo;
+	struct sockaddr_in myaddr;
 	struct sigaction sa;
 
-	memset( &hints, 0, sizeof( hints ) );
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-
-	if( getaddrinfo( NULL, PORT_NUMBER, &hints, &servinfo ) != 0 )
+	handshake_socket = socket( PF_INET, SOCK_STREAM, 0 );
+	if( handshake_socket == -1 )
 	{
-		logfile << "getaddrinfo() failure." << endl;
-		logfile.close();
+		syslog( LOG_INFO, "%s", "socket() failure" );
 		exit( EXIT_FAILURE );
 	}
-
-	// Bind to the first result possible.
-	struct addrinfo *p = NULL;
-	for( p = servinfo; p != NULL; p = p->ai_next )
+	else
 	{
-		handshake_socket = socket( p->ai_family, p->ai_socktype, p->ai_protocol );
-		if( handshake_socket == -1 )
-		{
-			continue;
-		}
-
-		int yes = 1;
-		if( setsockopt( handshake_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof( int ) ) == -1 )
-		{
-			exit( EXIT_FAILURE );
-		}
-
-		if( bind( handshake_socket, p->ai_addr, p->ai_addrlen ) == -1 )
-		{
-			close( handshake_socket );
-			continue;
-		}
-
-		break;
+		syslog( LOG_INFO, "%s", "socket() success" );
 	}
 
-	if( p == NULL )
+	int yes = 1;
+	if( setsockopt( handshake_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof( int ) ) == -1 )
 	{
-		logfile << "setsockopt() or bind() failure." << endl;
-		logfile.close();
+		syslog( LOG_INFO, "%s", "setsockopt() failure" );
+		close( handshake_socket );
 		exit( EXIT_FAILURE );
 	}
+	else
+	{
+		syslog( LOG_INFO, "%s", "setsockopt() success" );
+	}
 
-	freeaddrinfo( servinfo );
+	myaddr.sin_family = AF_INET; 				/* Host byte order */
+	myaddr.sin_port = htons( PORT_NUMBER );			/* Network byte order */
+	myaddr.sin_addr.s_addr = htonl( INADDR_ANY ); 		/* Get my IP address */
+	memset( &( myaddr.sin_zero ), '\0', 8 ); 			/* Zero it out */
+
+	/* Bind it to your IP address and a suitable port number. */
+	if( bind( handshake_socket, (struct sockaddr *)&myaddr, sizeof( struct sockaddr ) ) == -1 )
+	{
+		syslog( LOG_INFO, "%s", "bind() failure" );
+		close( handshake_socket );
+		exit( EXIT_FAILURE );
+	}
+	else
+	{
+		syslog( LOG_INFO, "%s", "bind() success" );
+	}
 
 	if( listen( handshake_socket, 10 ) == -1 )
 	{
-		logfile << "listen() failure." << endl;
-		logfile.close();
+		syslog( LOG_INFO, "%s", "listen() failure" );
+		close( handshake_socket );
 		exit( EXIT_FAILURE );
+	}
+	else
+	{
+		syslog( LOG_INFO, "%s", "listen() success" );
 	}
 
 	// Take care of zombie processes.
@@ -136,41 +129,67 @@ int main()
 	sa.sa_flags = SA_RESTART;
 	if( sigaction( SIGCHLD, &sa, NULL ) == -1 )
 	{
-		logfile << "sigaction() failure." << endl;
-		logfile.close();
+		syslog( LOG_INFO, "%s", "sigaction() failure" );
+		close( handshake_socket );
 		exit( EXIT_FAILURE );
+	}
+	else
+	{
+		syslog( LOG_INFO, "%s", "sigaction() success" );
 	}
 
 	while( 1 )
 	{
-		struct sockaddr_storage client_addr;
-		socklen_t sin_size = sizeof( client_addr );
-		int connection_socket = accept( handshake_socket, (struct sockaddr *)&client_addr, &sin_size );
+		struct sockaddr_in theiraddr;
+		socklen_t theiraddr_size = sizeof( struct sockaddr_in );
+
+		/* Accept the incoming connection request. */
+		int connection_socket = accept( handshake_socket, (struct sockaddr *)&theiraddr, &theiraddr_size );
 		if( connection_socket == -1 )
 		{
+			syslog( LOG_INFO, "%s", "accept() failure" );
 			continue;
+		}
+		else
+		{
+			syslog( LOG_INFO, "%s", "accept() success" );
 		}
 
 		if( !fork() )
 		{
 			char buffer[MAX_BUFFER_SIZE];
 			close( handshake_socket );	// Child doesn't need this socket.
-			
+
 			// Get the file name.
 			if( recv( connection_socket, buffer, MAX_BUFFER_SIZE, 0 ) == -1 )
 			{
-				logfile << "recv() failure: filename." << endl;
-				logfile.close();
+				syslog( LOG_INFO, "%s", "recv() failure, filename" );
+				close( connection_socket );
 				exit( EXIT_FAILURE );
+			}
+			else
+			{
+				syslog( LOG_INFO, "%s %s", "Filename", buffer );
 			}
 			string filename = buffer;
 
 			// Get type of file (text or binary).
 			if( recv( connection_socket, buffer, MAX_BUFFER_SIZE, 0 ) == -1 )
 			{
-				logfile << "recv() failure: file type." << endl;
-				logfile.close();
+				syslog( LOG_INFO, "%s", "recv() failure, file type" );
+				close( connection_socket );
 				exit( EXIT_FAILURE );
+			}
+			else
+			{
+				syslog( LOG_INFO, "%s %s", "File type", buffer );
+			}
+
+			// Delete the file if it exists
+			if( access( filename.c_str(), F_OK ) == 0 )
+			{
+				syslog( LOG_INFO, "%s", "unlink() called" );
+				unlink( filename.c_str() );
 			}
 
 			int size = 0;
@@ -180,13 +199,14 @@ int main()
 				ofile.open( filename.c_str(), ios::out );
 				if( ofile.fail() )
 				{
-					logfile << "Error opening file \'" << filename << "\'." << endl;
-					logfile.close();
+					syslog( LOG_INFO, "%s %s %s", "Couldn't open text file", get_current_dir_name(), filename.c_str() );
+					close( connection_socket );
 					exit( EXIT_FAILURE );
 				}
 				while( ( size = recv( connection_socket, buffer, MAX_BUFFER_SIZE, 0 ) ) > 0 )
 				{
-					ofile << buffer;
+					syslog( LOG_INFO, "%s :: %s", "write", buffer );
+					ofile.write( buffer, size );
 				}
 			}
 			else if( !strcmp( buffer, "binary" ) )
@@ -194,20 +214,23 @@ int main()
 				ofile.open( filename.c_str(), ios::out | ios::binary );
 				if( ofile.fail() )
 				{
-					logfile << "Error opening file \'" << filename << "\'." << endl;
-					logfile.close();
+					syslog( LOG_INFO, "%s %s", "Couldn't open binary file", filename.c_str() );
+					close( connection_socket );
 					exit( EXIT_FAILURE );
 				}
 				while( ( size = recv( connection_socket, buffer, MAX_BUFFER_SIZE, 0 ) ) > 0 )
 				{
+					syslog( LOG_INFO, "%s :: %s", "write", buffer );
 					ofile.write( buffer, size );
 				}
 			}
 			else
 			{
+				close( connection_socket );
 				exit( EXIT_FAILURE );
 			}
 
+			ofile.close();
 			close( connection_socket );
 			exit( EXIT_SUCCESS );
 		}
